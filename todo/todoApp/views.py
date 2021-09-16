951b50f11206e01d9f89cef785f6cd4115545f1e
Alezhha <callermanjohnson@gmail.com>
2021-09-16 16:25:11 +0600

    dnu mofoka

--- todo/todoApp/views.py
+++ todo/todoApp/views.py
@@ -1,4 +1,4 @@
-from django.http.response import HttpResponseRedirect
+from django.http.response import HttpResponseNotFound, HttpResponseRedirect
 from todoApp.models import Todo
 from django.shortcuts import render
 
@@ -15,3 +15,16 @@ def create(request):
         todo.description = request.POST.get('description')
         todo.save()
     return HttpResponseRedirect('/')
+
+def edit(request, id):
+    try:
+        todo = Todo.objects.get(id = id)
+        if request.method == 'POST':
+            todo.title = request.POST.get('title')
+            todo.description = request.POST.get('description')
+            return HttpResponseRedirect('/')
+        else:
+            return render(request, 'edit.html', {'todo':todo})
+    except Todo.DoesNotExist:
+        return HttpResponseNotFound
+        ("<h2>Задача не найдена</h2>")
